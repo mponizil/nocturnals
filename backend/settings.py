a2d98393dbcbcdf4974cbd7817143b1c946b4fa7
Misha Ponizil <misha.ponizil@gmail.com>
2012-02-21 22:11:22 -0500

    Oh shit created signup form that POSTs to Django and read. Gotta figure out how to organize Django and how to make jQuery Mobile and Backbone play nice.

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,4 @@
-# Django settings for ghost project.
+# Django settings for backend project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'ghost.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- backend/urls.py
+++ backend/urls.py
@@ -1,17 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ghost.views.home', name='home'),
-    # url(r'^ghost/', include('ghost.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^users/new$', 'account.views.new'),
 )
