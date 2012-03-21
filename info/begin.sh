4bf5c3a812c68daacb3092e0548a99da2dd4d593
Misha Ponizil <misha.ponizil@gmail.com>
2012-03-21 08:13:59 -0400

    Major overhaul of directory structure so deploying to and previewing on server is easy peasy.

---
+++ info/begin.sh
@@ -0,0 +1,6 @@
+cd ~/dev/python/nocturnals
+/Applications/TextMate.app/Contents/Resources/mate .
+open /Applications/Google\ Chrome.app --args --disable-web-security
+open /Developer/Applications/Xcode.app
+cd backend
+python manage.py runserver
